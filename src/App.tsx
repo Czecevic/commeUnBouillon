95ea5281d80af78f1a99bfba9de21d853474f554
zecevic charles <zeceviccharles@MacBook-Pro-von-zecevic.local>
2025-10-07 20:44:05 +0200

    change name page again

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="commeUnBouillon">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/commeUnBouillon/', 
+  base: '/commeUnBouillon/', // ← Important !
   build: {
-    outDir: "build",
+    outDir: "build", // ok si tu veux utiliser 'build' avec gh-pages
   },
   server: {
     proxy: {
